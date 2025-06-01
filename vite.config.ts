e32b92bb87cd94e0d842651c756534082969adb4
Khan Pallob <khanpallobforstudy@gmail.com>
2025-06-01 02:36:30 +0200

    vite.confg.ts Change

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets")
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
+  build: {
+    sourcemap: true, // ✅ This enables source maps for debugging
+  },
+});
   build: {
     outDir: "dist",
     sourcemap: false,
